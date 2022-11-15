bfc5ecfa5615c028fda65605ece04fddd6faface
farhan-alam2000 <alaamm.farhan@gmail.com>
2022-11-15 22:36:43 +0530

    Login Signup functionality

---
+++ routes/auth.js
@@ -0,0 +1,100 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const Author = require("../models/author");
+const bcrypt = require('bcrypt');
+
+//view registration page
+router.get("/register", async (req, res) => {
+    res.render('auth/register', {layout: "layouts/layout2"})
+})
+
+// REGISTER a new user
+router.post("/register", async (req, res) => {
+    try {
+        // console.log(req.body)
+        const pass = req.body.password;
+        const username = req.body.username;
+        const email = req.body.email;
+        console.log(email);
+        if(pass.length < 4)
+            res.status(400).json("Password must be atleast 4 characters long");
+
+        if(username.length == 0)
+            res.status(400).json("Enter username");
+        
+        if(email.length == 0)
+            res.status(400).json("Enter email");
+        
+        console.log(1);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+        console.log(2);
+        const user = await User.findOne({username: req.body.username});
+        if(user) {
+            res.redirect('/auth/register')
+        }
+
+        const user2 = await User.findOne({email: req.body.email});
+        if(user2) {
+            res.redirect('/auth/register')
+        }
+
+        console.log(3);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password : hashedPass
+        });
+        const newAuthor = new Author({
+            name: req.body.username
+        })
+
+        userPut = await newUser.save();  // users will be the collection
+        await newAuthor.save(); //user is also an author
+        
+        // in which this user will get added (MongoDB plural thingy)
+        // console.log(userPut);
+        // res.status(200).json(userPut);
+        res.redirect('/auth/login')
+    } catch (err) {
+        console.log("NOOOOOO");
+        // res.status(500).json(err);
+        res.redirect('/auth/register');
+    }
+});
+
+// view login page
+router.get("/login", async (req, res) => {
+    res.render('auth/login', {layout: "layouts/layout2"})
+})
+
+
+// LOGIN
+router.post("/login", async (req, res) => {
+    // try {
+    //     const user = await User.findOne({username: req.body.username});
+    //     if(!user) {
+    //         res.status(400).json("Wrong credentials, try again");
+    //     }
+
+    //     const validated = await bcrypt.compare(req.body.password, user.password);
+    //     if(!validated) { // passwords don't match
+    //         res.status(400).json("Wrong credentials, try again");
+    //     }
+        
+    //     // take everything in variable others except password.
+    //     const { password, ...others } = user._doc;
+    //     // res.status(200).json(others);
+    //     // res.status(200).redirect('index'); // only after authentication successful
+
+    // } catch (err) {
+    //     // res.status(500).json(err);
+    //     res.status(500).redirect('/auth/login');
+    // }
+});
+
+module.exports = router
+
+
+
--- server.js
+++ server.js
@@ -8,26 +8,39 @@ const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/authors')
-const bookRouter = require('./routes/books')
-
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
-
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+const authRouter = require('./routes/auth')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout', 'layouts/layout2')
+app.use(expressLayouts)
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(express.urlencoded( {extended: false} ))
+
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
+app.use('/auth', authRouter)
+
+// app.delete('/logout', (req, res) => {
+//   req.logOut((err)=>{
+//     if(err){
+//       return next(err)
+//     }
+//     res.redirect('/login')
+//   })
+// })
 
 app.listen(process.env.PORT || 3000)
